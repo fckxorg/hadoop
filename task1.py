3190f120eefad0789bbf9e10717a0b98cf229b2a
Maxim Kokryashkin <m.kokryashkin@tarantool.org>
2021-11-17 01:04:03 +0300

    adapt to older versions of python

--- task1.py
+++ task1.py
@@ -4,5 +4,5 @@ import subprocess
 
 command = ['hdfs', 'fsck', None, '-files', '-blocks', '-locations']
 command[2] = sys.argv[1]
-print(subprocess.run(command, stdout=subprocess.PIPE).stdout.decode('utf-8').split('\n')[1].split('from')[1].split()[0])
+print(subprocess.check_output(command).decode('utf-8').split('\n')[1].split('from')[1].split()[0])
 
