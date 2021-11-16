e2ff116d6a1d28aa4f6ed28c6ef2d711fc2adf1b
Maxim Kokryashkin <m.kokryashkin@tarantool.org>
2021-11-17 02:03:14 +0300

    fix

--- task1.py
+++ task1.py
@@ -7,12 +7,11 @@ command = ['hdfs', 'fsck', sys.argv[1], '-files', '-blocks', '-locations']
 HDFS output looks like the following:
 
 ```
-FSCK started by pd2021006 (auth:SIMPLE) from /93.175.29.107 for path /datasets/stop_words_en.txt at Tue Nov 16 22:17:49 UTC 2021
-/datasets/stop_words_en.txt 1914 bytes, 1 block(s):  OK
+0. BP-1135830572-93.175.29.106-1493426010892:blk_1074209870_469301 len=1914 Live_repl=2 [DatanodeInfoWithStorage[93.175.29.111:50010,DS-0618707d-c00c-4563-b7d8-ec217f4c972c,DISK], DatanodeInfoWithStorage[93.175.29.109:50010,DS-3b63d425-0f0e-45be-b954-f090a313119d,DISK]]
 ```
 """
 print(subprocess.check_output(command).decode('utf-8')
-                                      .split('\n')[0]
-                                      .split('from')[1]
-                                      .split()[0][1:])
+                                      .split('\n')[2]
+                                      .split('DatanodeInfoWithStorage')[1]
+                                      .split(':')[0][1:]
 
