366522f09423d570436153eb3d6c1d504a8874ce
Maxim Kokryashkin <m.kokryashkin@tarantool.org>
2021-11-23 00:39:03 +0300

    fix

--- task5.py
+++ task5.py
@@ -24,7 +24,9 @@ for block in blocks_info:
     ssh_host = 'hdfsuser@' + node
     ssh_command = ['sudo', '-u', 'hdfsuser', 'ssh', ssh_host, 'find', '/dfs', '-name', blk_id]
     path = subprocess.check_output(ssh_command).decode('utf-8').split()[0]
-    print(path)
+    ssh_command = ssh_command[:5] + ['stat', path, '-c', '"%b %B"']
+    size = subprocess.check_output(ssh_command).decode('utf-8')
+    print(size)
 
 delete_remote = ['hdfs', 'dfs', '-rm', '/tmp/kok.tmp']
 subprocess.check_output(delete_remote)
